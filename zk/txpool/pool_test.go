d5ae61330f249cc8920ee9821d2cd71d6efa63ba
Ji Hwan <jkim@polygon.technology>
2024-10-31 21:42:13 +0900

    test: fix insufficient funds to test address
    Signed-off-by: Ji Hwan <jkim@polygon.technology>

--- zk/txpool/pool_test.go
+++ zk/txpool/pool_test.go
@@ -3,14 +3,12 @@ package txpool
 import (
 	"context"
 	"fmt"
-	"math/bits"
 	"testing"
 	"time"
 
 	"github.com/holiman/uint256"
 	"github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/common/datadir"
-	"github.com/ledgerwatch/erigon-lib/common/length"
 	"github.com/ledgerwatch/erigon-lib/common/u256"
 	"github.com/ledgerwatch/erigon-lib/gointerfaces"
 	"github.com/ledgerwatch/erigon-lib/gointerfaces/remote"
@@ -24,91 +22,30 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func EncodeAccountBytesV3(nonce uint64, balance *uint256.Int, hash []byte, incarnation uint64) []byte {
-	l := int(1)
-	if nonce > 0 {
-		l += common.BitLenToByteLen(bits.Len64(nonce))
-	}
-	l++
-	if !balance.IsZero() {
-		l += balance.ByteLen()
-	}
-	l++
-	if len(hash) == length.Hash {
-		l += 32
-	}
-	l++
-	if incarnation > 0 {
-		l += common.BitLenToByteLen(bits.Len64(incarnation))
-	}
-	value := make([]byte, l)
-	pos := 0
-
-	if nonce == 0 {
-		value[pos] = 0
-		pos++
-	} else {
-		nonceBytes := common.BitLenToByteLen(bits.Len64(nonce))
-		value[pos] = byte(nonceBytes)
-		var nonce = nonce
-		for i := nonceBytes; i > 0; i-- {
-			value[pos+i] = byte(nonce)
-			nonce >>= 8
-		}
-		pos += nonceBytes + 1
-	}
-	if balance.IsZero() {
-		value[pos] = 0
-		pos++
-	} else {
-		balanceBytes := balance.ByteLen()
-		value[pos] = byte(balanceBytes)
-		pos++
-		balance.WriteToSlice(value[pos : pos+balanceBytes])
-		pos += balanceBytes
-	}
-	if len(hash) == 0 {
-		value[pos] = 0
-		pos++
-	} else {
-		value[pos] = 32
-		pos++
-		copy(value[pos:pos+32], hash)
-		pos += 32
-	}
-	if incarnation == 0 {
-		value[pos] = 0
-	} else {
-		incBytes := common.BitLenToByteLen(bits.Len64(incarnation))
-		value[pos] = byte(incBytes)
-		var inc = incarnation
-		for i := incBytes; i > 0; i-- {
-			value[pos+i] = byte(inc)
-			inc >>= 8
-		}
-	}
-	return value
-}
-
 func TestNonceFromAddress(t *testing.T) {
 	assert, require := assert.New(t), require.New(t)
 	ch := make(chan types.Announcements, 100)
-
+	// coreDB := memdb.NewTestPoolDB(t)
 	_, coreDB, _ := temporaltest.NewTestDB(t, datadir.New(t.TempDir()))
+	defer coreDB.Close()
+
 	db := memdb.NewTestPoolDB(t)
 
 	path := fmt.Sprintf("/tmp/db-test-%v", time.Now().UTC().Format(time.RFC3339Nano))
 
 	txPoolDB := newTestTxPoolDB(t, path)
+	defer txPoolDB.Close()
 	aclsDB := newTestACLDB(t, path)
+	defer aclsDB.Close()
 
 	// Check if the dbs are created
 	require.NotNil(t, txPoolDB)
 	require.NotNil(t, aclsDB)
 
 	cfg := txpoolcfg.DefaultConfig
+	ethCfg := &ethconfig.Defaults
 	sendersCache := kvcache.New(kvcache.DefaultCoherentConfig)
-	pool, err := New(ch, coreDB, cfg, &ethconfig.Defaults, sendersCache, *u256.N1, nil, nil, aclsDB)
+	pool, err := New(ch, coreDB, cfg, ethCfg, sendersCache, *u256.N1, nil, nil, aclsDB)
 	assert.NoError(err)
 	require.True(pool != nil)
 	ctx := context.Background()
@@ -116,6 +53,17 @@ func TestNonceFromAddress(t *testing.T) {
 	pendingBaseFee := uint64(200000)
 	// start blocks from 0, set empty hash - then kvcache will also work on this
 	h1 := gointerfaces.ConvertHashToH256([32]byte{})
+
+	var addr [20]byte
+	addr[0] = 1
+
+	// Add 1 eth to the user account, as a part of change
+	v := make([]byte, types.EncodeSenderLengthForStorage(2, *uint256.NewInt(18 * common.Ether)))
+	types.EncodeSender(2, *uint256.NewInt(18 * common.Ether), v)
+	tx, err := db.BeginRw(ctx)
+	require.NoError(err)
+	defer tx.Rollback()
+
 	change := &remote.StateChangeBatch{
 		StateVersionId:      stateVersionID,
 		PendingBlockBaseFee: pendingBaseFee,
@@ -124,18 +72,11 @@ func TestNonceFromAddress(t *testing.T) {
 			{BlockHeight: 0, BlockHash: h1},
 		},
 	}
-	var addr [20]byte
-	addr[0] = 1
-	// addr := common.HexToAddress("0x1")
-	v := EncodeAccountBytesV3(2060, uint256.NewInt(10*common.Ether), make([]byte, 32), 1)
 	change.ChangeBatch[0].Changes = append(change.ChangeBatch[0].Changes, &remote.AccountChange{
 		Action:  remote.Action_UPSERT,
 		Address: gointerfaces.ConvertAddressToH160(addr),
 		Data:    v,
 	})
-	tx, err := db.BeginRw(ctx)
-	require.NoError(err)
-	defer tx.Rollback()
 	err = pool.OnNewBlock(ctx, change, types.TxSlots{}, types.TxSlots{}, tx)
 	assert.NoError(err)
 
@@ -145,7 +86,7 @@ func TestNonceFromAddress(t *testing.T) {
 			Tip:    *uint256.NewInt(300000),
 			FeeCap: *uint256.NewInt(300000),
 			Gas:    100000,
-			Nonce:  2061,
+			Nonce:  3,
 		}
 		txSlot1.IDHash[0] = 1
 		txSlots.Append(txSlot1, addr[:], true)
@@ -163,14 +104,14 @@ func TestNonceFromAddress(t *testing.T) {
 			Tip:    *uint256.NewInt(300000),
 			FeeCap: *uint256.NewInt(300000),
 			Gas:    100000,
-			Nonce:  2062,
+			Nonce:  4,
 		}
 		txSlot2.IDHash[0] = 2
 		txSlot3 := &types.TxSlot{
 			Tip:    *uint256.NewInt(300000),
 			FeeCap: *uint256.NewInt(300000),
 			Gas:    100000,
-			Nonce:  2063,
+			Nonce:  6,
 		}
 		txSlot3.IDHash[0] = 3
 		txSlots.Append(txSlot2, addr[:], true)
@@ -182,7 +123,7 @@ func TestNonceFromAddress(t *testing.T) {
 		}
 		nonce, ok := pool.NonceFromAddress(addr)
 		assert.True(ok)
-		assert.Equal(uint64(2063), nonce)
+		assert.Equal(uint64(6), nonce)
 	}
 	// test too expensive tx
 	{
@@ -191,7 +132,7 @@ func TestNonceFromAddress(t *testing.T) {
 			Tip:    *uint256.NewInt(300000),
 			FeeCap: *uint256.NewInt(9 * common.Ether),
 			Gas:    100000,
-			Nonce:  2061,
+			Nonce:  3,
 		}
 		txSlot1.IDHash[0] = 4
 		txSlots.Append(txSlot1, addr[:], true)
@@ -209,7 +150,7 @@ func TestNonceFromAddress(t *testing.T) {
 			Tip:    *uint256.NewInt(300000),
 			FeeCap: *uint256.NewInt(300000),
 			Gas:    100000,
-			Nonce:  0,
+			Nonce:  1,
 		}
 		txSlot1.IDHash[0] = 5
 		txSlots.Append(txSlot1, addr[:], true)
@@ -220,5 +161,3 @@ func TestNonceFromAddress(t *testing.T) {
 		}
 	}
 }
-
-//219
